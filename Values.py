3d2889775d377dacc4e3adc0bd411f4926349663
Joseph Duppstadt <JoeCraigDuppstadt@gmail.com>
2021-04-18 18:29:07 -0400

    Initial Commit

---
+++ Values.py
@@ -0,0 +1,14 @@
+class Values:
+    def __init__(self, c, neg, neu, pos):
+        self.compound = c
+        self.negative = neg
+        self.neutral = neu
+        self.positive = pos
+
+
+    def __str__(self):
+        return "Compund: " + str(self.compound) + " Neg: " + str(self.negative) +  " Neu: " + str(self.neutral) + " Pos: " + str(self.positive)
+
+
+
+
